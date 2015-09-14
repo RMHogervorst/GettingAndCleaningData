c1912a95058f758b44eaf3299f1ca1bc106bddd8
Roel Hogervorst <hogervorst.rm@gmail.com>
2015-09-14 16:24:46 +0200

    fread omgezet in data.table, want fread werkte niet., gaf error ahv seperator

--- run_analysis.R
+++ run_analysis.R
@@ -1,18 +1,18 @@
 run_analysis<-function() {
        library(data.table)
-        ##zet alles om in fread en dan rbindlist
+        ##zet alles om in  en dan rbindlist
 #      1.  Merges the training and the test sets to create one data set.
 #   load all parts of the testset
-        testParticipant <- fread("data/test/subject_test.txt")
-        testX <- fread("data/test/X_test.txt")
-        testY <- fread("data/test/y_test.txt")
+        testParticipant <- read.table("data/test/subject_test.txt")
+        testX <- read.table("data/test/X_test.txt")
+        testY <- read.table("data/test/y_test.txt")
 #       load parts of the trainingset
-        trainParticipant <- fread("data/train/subject_train.txt")
-        trainX <- fread("data/train/X_train.txt")
-        trainY <- fread("data/train/y_train.txt")
+        trainParticipant <- read.table("data/train/subject_train.txt")
+        trainX <- read.table("data/train/X_train.txt")
+        trainY <- read.table("data/train/y_train.txt")
 #       load the features
-        features <- fread("data/features.txt")
-        activityLabels <- fread("data/activity_labels.txt")
+        features <- read.table("data/features.txt")
+        activityLabels <- read.table("data/activity_labels.txt")
         
         Participants <- rbind(testParticipant, trainParticipant)
         colnames(Participants) <- "Participants"
