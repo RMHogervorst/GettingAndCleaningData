c50b5a883c23653183e34574a3d9f45638d07fac
Roel Hogervorst <hogervorst.rm@gmail.com>
2015-09-21 16:03:41 +0200

    aanroepen van data.table is naar onderen gegaan. de dplyr functies werken hier. het eindresultaat van deze functie is een apart dataset met de gemiddelden per persoon per activiteit voor 3 variabelen. Maar nog niet met tidy namen.

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,5 @@
 run_analysis<-function() {
        library(data.table)
-        ##zet alles om in  en dan rbindlist
 #      1.  Merges the training and the test sets to create one data set.
 #   load all parts of the testset
         testParticipant <- read.table("data/test/subject_test.txt")
@@ -28,11 +27,65 @@ run_analysis<-function() {
         
 #       2. Extracts only the measurements on the mean 
         #and standard deviation for each measurement. 
+        #       3. Uses descriptive activity names to name the activities in the data set
+        #happened in the first steps
+        #       4. Appropriately labels the data set with descriptive variable names. 
+        #        
+        #       5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+        rm(activityLabels,features,Participants, label,
+           testParticipant,testX,testY,trainParticipant,
+           trainX,trainY) #cleaning up the environment.
         library(dplyr)
-
-        dt <- select(data, matches("mean|std"))        
-#       3. Uses descriptive activity names to name the activities in the data set
-#       4. Appropriately labels the data set with descriptive variable names. 
-#        
-#       5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-}
+        data %>%select(data, Participants, label, matches("mean|std"))%>% 
+                group_by(Participants, label) %>%
+                summarise(mean)%>% #mean of values. 
+                select(
+                   ParticipantNumber = Participants, 
+                   Activity = label,
+                   AveragetBodyAccMean=sum(`tBodyAcc-mean()-X`,`tBodyAcc-mean()-Y`,`tBodyAcc-mean()-Z`)/3,
+                   AveragetBodyAccSTD = sum(`tBodyAcc-std()-X`,`tBodyAcc-std()-Y`,`tBodyAcc-std()-Z`)/3,
+                   AveragetGravityAccMean = sum(`tGravityAcc-mean()-X`,`tGravityAcc-mean()-Y`,`tGravityAcc-mean()-Z`)/3,
+                   AveragetGravityAccSTD = sum(`tGravityAcc-std()-X`,`tGravityAcc-std()-Y`,`tGravityAcc-std()-Z`)/3,
+                   AveragetBodyAccJerkMean = sum(`tBodyAccJerk-mean()-X`,`tBodyAccJerk-mean()-Y`,`tBodyAccJerk-mean()-Z`)/3,
+                   AveragetBodyAccJerkSTD = sum(`tBodyAccJerk-std()-X`,`tBodyAcc-std()-Y`, `tBodyAcc-std()-Z`)/3,
+                   AveragetBodyGyroMean = sum(`tBodyGyro-mean()-X`,`tBodyGyro-mean()-Y`,`tBodyGyro-mean()-Z`)/3,
+                   AveragetBodyGyroSTD = sum(`tBodyGyro-std()-X`,`tBodyGyro-std()-Y`,`tBodyGyro-std()-Z`)/3,
+                   AveragetBodyGyroJerkMean = sum(`tBodyGyroJerk-mean()-X`,`tBodyGyroJerk-mean()-Y`,`tBodyGyroJerk-mean()-Z`)/3,
+                   AveragetBodyGyroJerkSTD = sum(`tBodyGyroJerk-std()-X`,`tBodyGyroJerk-std()-Y`,`tBodyGyroJerk-std()-Z`)/3,
+                   AveragetBodyGyroJerkMagMean =sum(`tBodyGyroJerk-mean()-X`,`tBodyGyroJerk-mean()-Y`,`tBodyGyroJerk-mean()-Z`)/3,
+                   AveragetBodyGyroJerkMagSTD = sum(`tBodyGyroJerk-std()-X`,`tBodyGyroJerk-std()-Y`,`tBodyGyroJerk-std()-Z`)/3,
+                   tBodyAccMagMean = `tBodyAccMag-mean()`,
+                   tBodyAccMagSTD = `tBodyAccMag-std()`,
+                   tBodyAccJerkMagMean =`tBodyAccJerkMag-mean()`,
+                   tBodyAccJerkMagSTD =`tBodyAccJerkMag-std()`,
+                   tBodyGyroJerkMagMean= `tBodyGyroJerkMag-mean()`,
+                   tBodyGyroJerkMagSTD = `tBodyGyroJerkMag-std()`,
+                   AveragefBodyAccMean =sum(`fBodyAcc-mean()-X`,`fBodyAcc-mean()-Y`,`fBodyAcc-mean()-Z`)/3,
+                   AveragefBodyAccSTD = sum(`fBodyAcc-std()-X`,`fBodyAcc-std()-Y`,`fBodyAcc-std()-Z`)/3,
+                   AveragefBodyAccMeanFreq = sum(`fBodyAcc-meanFreq()-X`, `fBodyAcc-meanFreq()-Y`,`fBodyAcc-meanFreq()-Z`)/3,
+                   AveragefBodyAccJerkMean = sum(`fBodyAccJerk-mean()-X`,`fBodyAccJerk-mean()-Y`,`fBodyAccJerk-mean()-Z`)/3,
+                   AveragefBodyAccJerkSTD = sum(`fBodyAccJerk-std()-X`,`fBodyAccJerk-std()-Y`,`fBodyAccJerk-std()-Z`)/3,
+                   fBodyAccMagMean = `fBodyAccMag-mean()`,
+                   fBodyAccMagSTD = `fBodyAccMag-std()`,
+                   fBodyAccJerkMeanFreqMean = sum(`fBodyAccJerk-meanFreq()-X`, `fBodyAccJerk-meanFreq()-Y`,`fBodyAccJerk-meanFreq()-Z`)/3,
+                   fBodyBodyAccJerkMagMEAN =`fBodyBodyAccJerkMag-mean()`,
+                   fBodyBodyAccJerkMagSTD = `fBodyBodyAccJerkMag-std()`,
+                   AveragefBodyGyroMean = sum(`fBodyGyro-mean()-X`,`fBodyGyro-mean()-Y`,`fBodyGyro-mean()-Z`)/3,
+                   AveragefBodyGyroSTD = sum(`fBodyGyro-std()-X`,`fBodyGyro-std()-Y`,`fBodyGyro-std()-Z`)/3,
+                   AveragefBodyGyroMEANFreq = sum(`fBodyGyro-meanFreq()-X`,`fBodyGyro-meanFreq()-Y`, `fBodyGyro-meanFreq()-Z`)/3,                   
+                   fBodyAccMagMeanFreq = `fBodyAccMag-meanFreq()`,                        
+                   BodyBodyAccJerkMagMEANFreq = `fBodyBodyAccJerkMag-meanFreq()`,                    
+                   fBodyBodyGyroMagSTD = `fBodyBodyGyroMag-std()` , 
+                   fBodyBodyGyroMagMEANFreq = `fBodyBodyGyroMag-meanFreq()` ,         
+                   fBodyBodyGyroJerkMagMEAN = `fBodyBodyGyroJerkMag-mean()`,
+                   fBodyBodyGyroJerkMagSTD = `fBodyBodyGyroJerkMag-std()`,
+                   fBodyBodyGyroJerkMagMEANFreq = `fBodyBodyGyroJerkMag-meanFreq()`,      
+                   angletBodyAccMeanGravity =`angle(tBodyAccMean,gravity)`         ,
+                   angletBodyAccJerkMeanGravityMean=`angle(tBodyAccJerkMean),gravityMean)`,
+                   angletBodyGyroMEANGravityMean =`angle(tBodyGyroMean,gravityMean)`,
+                   angletBodyGyroJerkMeanGravityMean =`angle(tBodyGyroJerkMean,gravityMean)`,
+                   AverageangleGravityMean =sum(`angle(X,gravityMean)`,`angle(Y,gravityMean)` ,`angle(Z,gravityMean)`)/3) %>%
+                
+        
+        
+        }
---
+++ week3quizscripts.R
@@ -0,0 +1,18 @@
+#week3 quiz
+
+link<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
+download.file(link, destfile="pums.csv")
+pums<-read.csv("pums.csv")
+#Create a logical vector that identifies the households on greater 
+#than 10 acres who sold more than $10,000 worth of agriculture products. 
+#Assign that logical vector to the variable agricultureLogical
+agricultureLogical <- c(ACR=3, AGS =6)
+
+library(dplyr)
+head(filter(pums, ACR ==3, AGS ==6),3)
+
+library(jpeg)
+fotolink<-"https://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg"        
+download.file(fotolink, destfile = "jeff.jpg", mode = "wb")
+jeff<-readJPEG(source="jeff.jpg", native=T)        
+quantile(jeff, probs = c(.3, .8))
